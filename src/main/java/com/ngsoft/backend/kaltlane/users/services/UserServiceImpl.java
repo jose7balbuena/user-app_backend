26f7168199e12dce5bdc5b38431ae5e55866eeaa
jose7Rodriguez <jose7.balbuena@gmail.com>
2026-01-20 09:59:38 -0600

    Se quitan liberias que no se usan en servicio de usuario

--- src/main/java/com/ngsoft/backend/kaltlane/users/services/UserServiceImpl.java
+++ src/main/java/com/ngsoft/backend/kaltlane/users/services/UserServiceImpl.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
